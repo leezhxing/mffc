c1b53b0e93c59a74d7cbacb564978a542530e7d8
leezhxing <leezhxing@163.com>
2015-12-15 23:37:59 +0800

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -4,9 +4,9 @@ return [
 
   'host' => 'smtp.163.com',
 
-  'username' => 'leezhxing@163.com',
+  'username' => 'lee@163.com',
 
-  'password' => '772556536lzx163',
+  'password' => 'asdasd',
 
   'secure' => ''
 
